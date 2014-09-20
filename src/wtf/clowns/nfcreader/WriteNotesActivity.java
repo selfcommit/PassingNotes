1bedf1d0851fd4294cfd827fa763077ad1b0cd99
doboyle <dan@stackoverflow.com>
2014-09-20 00:02:59 -0400

    Removed First attempt at Sql Helper

--- src/wtf/clowns/nfcreader/WriteNotesActivity.java
+++ src/wtf/clowns/nfcreader/WriteNotesActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class WriteNotesActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState){
