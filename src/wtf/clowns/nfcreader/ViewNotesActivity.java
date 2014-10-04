b12a03d5ba86ca73a9a331ddd9d006a5a3fb2298
doboyle <dan@stackoverflow.com>
2014-10-04 14:04:04 -0400

    View Notes Test List
    View Notes page now displays a sample array of Strings.  ToDo: pull
    array from strings from SQLite

--- src/wtf/clowns/nfcreader/ViewNotesActivity.java
+++ src/wtf/clowns/nfcreader/ViewNotesActivity.java
@@ -3,14 +3,35 @@ package wtf.clowns.nfcreader;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class ViewNotesActivity extends Activity{
+	private notesDBHelper db = null;
+	ListView notesList;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_notes);
+		
+		//Grab object form layout XML
+		notesList = (ListView) findViewById(R.id.Noteslist);
+		String[] testlist = new String[] {"First Note", "Second Note", "Third Note"};
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1,testlist);
+		
+		notesList.setAdapter(adapter);
+		
+		db = new notesDBHelper(this);
+		//String query = String.format("INSERT INTO notes (title, notetext) VALUES ('%s', '%s')", noteText, noteText);
+    	
+		//db.getWritableDatabase().rawQuery(query, null);
+		
 	}
 	
     @Override
